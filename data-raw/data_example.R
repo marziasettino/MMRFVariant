2e207f990a4f02faa32470ba6f0c2f251bd209bb
marziasettino <marzia.settino@gmail.com>
2021-11-25 16:53:04 +0100

    commit 2

---
+++ data-raw/data_example.R
@@ -0,0 +1,5 @@
+## code to prepare `data_example` dataset goes here
+
+usethis::use_data(variant.ann.example, overwrite = TRUE)
+usethis::use_data(trt.example, overwrite = TRUE)
+usethis::use_data(patient.example, overwrite = TRUE)
